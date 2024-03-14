cdfd892769d70f57ed30817461ae634d5817994e
mmKeerthana2001 <162995410+mmKeerthana2001@users.noreply.github.com>
2024-03-14 16:40:27 +0530

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,19 @@
+
+#ways to print
+n=30
+txt="i have {} friends"
+print(txt.format(n))
+
+n=30
+print("i have {} friends".format(n))
+
+n=30
+print("i have %d friends"%(n))
+
+n=30
+print("i have {} friends".format(n))
+
+n=30
+print(f"i have {n} friends")
+
+#output="i have 30 friends"
