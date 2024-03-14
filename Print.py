82e8658e02cda36abcb4e577d6aadc1e65b64658
Shivakumara K Madegowda <shivakumara.madegowda@gmail.com>
2024-03-14 17:37:33 +0530

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,6 +1,7 @@
 
 #ways to print
 n=30
+
 txt="i have {} friends"
 print(txt.format(n))
 
@@ -11,9 +12,8 @@ n=30
 print("i have %d friends"%(n))
 
 n=30
-print("i have {} friends".format(n))
-
-n=30
-print(f"i have {n} friends")
+print(f "i have {n} friends")
 
 #output="i have 30 friends"
+
+# explore the what do we call the above format representaion. 
