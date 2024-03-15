32a2e17cb38bb68473bf52bc6782573763c62835
Jeevika19-ui <132327665+Jeevika19-ui@users.noreply.github.com>
2024-03-15 17:55:54 +0530

    Add files via upload

---
+++ Jeevika/tuple.py
@@ -0,0 +1,38 @@
+
+'''
+import os
+# to list all the methods present in this module
+print(dir(os))
+#get current working directory
+print(os.getcwd())
+'''
+
+
+
+t1=(1,1,1,1,1,1)
+first_ele=t1[0]
+if(t1.count(first_ele))==len(t1):
+  print("true")
+else:
+ print("false") 
+    
+'''
+t3=set(t1)
+if t1[0]==t1[1]:
+ print("equal")
+ if t1[1]==t1[2]:
+  print("1 and 2 are equal") 
+ else:
+  print(" data are not same")
+  '''
+ 
+t2=(1,1,1,1)
+t4=(2,3,4,5)
+t5=t2
+t2=t4
+print(t2)
+print(t1)
+  
+  
+
+
