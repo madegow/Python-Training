527702cf8a06ad801e3410e0de64ab8819cfc0bc
mmKeerthana2001 <162995410+mmKeerthana2001@users.noreply.github.com>
2024-03-15 17:53:35 +0530

    Add files via upload

---
+++ Keerthana/Task1.py
@@ -0,0 +1,23 @@
+
+#concatenate
+n=30
+print("I have "+ str(n) +" friends")
+
+#Print with the ‘str.format()’ method
+txt="i have {} friends"
+print(txt.format(n))
+
+n=30
+print("i have {} friends".format(n))
+
+#print with string formatting
+n=30
+print("i have %d friends"%(n))
+
+#Print with the ‘f-string’ syntax
+n=30
+print(f "i have {n} friends")
+
+#output="i have 30 friends"
+
+# explore the what do we call the above format representaion. 
---
+++ Keerthana/Task3.py
@@ -0,0 +1,50 @@
+'''
+import os
+
+def get_file_owner(file_path):
+    try:
+        file_info = os.stat(file_path)
+        owner_sid = file_info.st_uid
+        print(owner_sid)
+        owner_name = None
+        with os.popen(f'wmic useraccount where sid="{owner_sid}" get name /value') as stream:
+            for line in stream:
+                if line.strip():
+                    owner_name = line.split('=')[1].strip()
+                    break
+        return owner_name
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+file_path =(r"C:\\Users\\kisho\\OneDrive\\Desktop\\Python\\contents.rtf")  
+owner = get_file_owner(file_path)
+if owner:
+    print(f"The owner of {file_path} is {owner}.")
+else:
+    print(f"Failed to retrieve the owner of {file_path}.")
+'''
+#Name of a file
+import os
+file_path = ("D:\\DotNET\\NetworkScanning\\mylist.py")
+file_name = os.path.basename(file_path)
+print("File Name:", file_name)
+
+#File type of a file
+def get_file_type(file_path):
+    file_extension = os.path.splitext(file_path)[1]
+    return file_extension.lower()
+
+
+file_type = get_file_type(file_path)
+print("File Type:", file_type)
+
+#size,modification time and creation time
+file_path = os.stat("D:\\DotNET\\NetworkScanning\\mylist.py")
+print(type(file_path))
+print(f"size:{file_path.st_size}bytes")
+print(f"Modification time:{file_path.st_mtime}")
+print(f"creation time:{file_path.st_ctime}")
+
+
+
