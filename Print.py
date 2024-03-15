41748212754ee864bfc99e4c38a514de50347d3a
mmKeerthana2001 <162995410+mmKeerthana2001@users.noreply.github.com>
2024-03-15 14:58:02 +0530

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,18 +1,20 @@
 
-#ways to print
+#concatenate
 n=30
-
 print("I have "+ str(n) +" friends")
 
+#Print with the ‘str.format()’ method
 txt="i have {} friends"
 print(txt.format(n))
 
 n=30
 print("i have {} friends".format(n))
 
+#print with string formatting
 n=30
 print("i have %d friends"%(n))
 
+#Print with the ‘f-string’ syntax
 n=30
 print(f "i have {n} friends")
 
