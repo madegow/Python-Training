16d3e24e12b1c7f20ad88fceb47ca67bfa9278a0
Shivakumara K Madegowda <shivakumara.madegowda@gmail.com>
2024-03-28 14:58:06 +0530

    Update networkscanning.py

--- Shivu/networkscanning.py
+++ Shivu/networkscanning.py
@@ -1,6 +1,7 @@
+from uu import Error
 import netifaces
 import scapy.all as scapy
-import nmap
+
 
 print("Starting ...")
 
@@ -8,38 +9,75 @@ class nvpt:
 	def __init__(self, d_gatewaytmp="192.168.0.1"):
 		self.d_gateway = d_gatewaytmp
 		self.result = list()
-		self.test_result = ['192.168.0.102', '192.168.0.105', '192.168.0.108', '192.168.0.109', '192.168.0.110', '192.168.0.111', '192.168.0.117', '192.168.0.118', '192.168.0.121', '192.168.0.125', '192.168.0.126', '192.168.0.127', '192.168.0.128', '192.168.0.129', '192.168.0.130', '192.168.0.133', '192.168.0.137', '192.168.0.139', '192.168.0.140']
+		
 		
 	def scannetwork(self):
-		octet_list3 = self.d_gateway.split('.')[0:3]
-		ip_string = '.'.join(octet_list3)
-	
-		for last_octet in range(100,254):
-			ip = ip_string+'.'+str(last_octet)
-			arp_frame = scapy.ARP(pdst=ip)
-			broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-			request_frame = broadcast_frame/arp_frame
-			scan_result = scapy.srp(request_frame, timeout = 1, verbose = False)[0]
-			for i in range(0, len(scan_result)):
-				client_dict = {"ip" : scan_result[i][1].psrc, "mac" : scan_result[i][1].hwsrc}
-				if bool(client_dict):
-					self.result.append(client_dict['ip'])
-		print(self.result)
+		ip=self.d_gateway
+		first_three_octet = ip.split('.')[:3]
+		ip_string = '.'.join(first_three_octet)
+		ip_t = ip_string+'.'+'0'+'/24'
+		print(ip_t, type(ip_t))
+		arp_frame = scapy.ARP(pdst=ip_t)
+		broadcast_frame = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+		request_frame = broadcast_frame/arp_frame
+		print(request_frame)
+		answered = scapy.srp(request_frame, timeout = 5, verbose = True)[0]
+		print("Ansered =====", answered.summary())
+		
+		exit()
+		first_three_octet = ip.split('.')[:3]
+		ip_string = '.'.join(first_three_octet)
+		print(ip_string)
+		for i in range(100,254):
+			try:
+				ip_t = ip_string+'.'+str(i)
+				print(ip_t, type(ip_t))
+				arp_frame = scapy.ARP(pdst=ip_t)
+				broadcast_frame = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+				request_frame = broadcast_frame/arp_frame
+				print(request_frame)
+				answered, unanswered = scapy.srp(request_frame, timeout = 5, verbose = True)[0]
+				print("Ansered =====", answered.summary())
+				print("UnAnsered =====", unanswered.summary())
+			except:
+				print(i, Error)
+				
+			
+		exit(0)
+		arp_frame = scapy.ARP(pdst=ip)
+		broadcast_frame = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+		request_frame = broadcast_frame/arp_frame
+		
+		answered, unanswered = scapy.srp(request_frame, timeout = 5, verbose = False)[0]
+		print("Ansered =====", answered.summary())
+		print("UnAnsered =====", unanswered.summary())
+
+		my_lambda = lambda s,r: self.result.append(r['ARP'].psrc)
+		answered.filter(my_lambda)
+		print(type(self.result))
+		print(self.result.remove(self.d_gateway))
+		self.result.remove("192.168.0.108")
 		return self.result
 	
-	def scanmachine(self):
-		#if not self.result:
-		#	self.scannetwork()
-			
+	def portscan(self):
 		for ip in self.result:
-			scanner = nmap.PortScanner()
-			scanner.scan(ip, arguments='-sS -O')
-			print("hosts ---", scanner.all_hosts())
-			for host in scanner.all_hosts():
-				print(scanner[host])
-			
-		
+			print(ip)
+			ans = scapy.sr1(scapy.IP(dst=ip)/scapy.ICMP(), timeout=2)
+			print(type(ans))
+			if len(ans) == 0:
+				print(ip+": not pinging")
+			else:
+				print(ip+": successful in pinging")
 
+
+		
+def get_gateway():
+	routes=	scapy.conf.route.routes
+	# Find the default gateway
+	for route in routes:
+		if route[0] == 0 and route[1] == 0:
+			return route[2]
+	return None
 
 
 if __name__ == "__main__":
@@ -47,11 +85,10 @@ if __name__ == "__main__":
    nwlist = netifaces.gateways()
    NW = networks(str(nwlist['default'][netifaces.AF_INET][0]))
    """
-   nwlist = netifaces.interfaces()
-   gateways = netifaces.gateways()
-   default_gateway=gateways[2][0][0]
-   print(default_gateway)
-   net = nvpt()
+   #gateways = netifaces.gateways()
+   default_gateway=get_gateway()
+   #print(default_gateway)
+   net = nvpt(default_gateway)
    net.scannetwork()
-   net.scanmachine()
+   net.portscan()
    exit(0)
